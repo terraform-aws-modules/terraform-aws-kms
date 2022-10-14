7b85239466b365c31d7e976f82964789d4215c0e
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-14 08:54:13 -0400

    fix: Modify `key_service_principals` to be generic to support conditions for confused deputy problem (#7)
    Co-authored-by: Anton Babenko <anton@antonbabenko.com>

--- main.tf
+++ main.tf
@@ -244,16 +244,42 @@ data "aws_iam_policy_document" "this" {
   }
 
   dynamic "statement" {
-    for_each = var.key_service_principals
+    for_each = var.key_statements
 
     content {
-      sid       = statement.value.sid
-      actions   = statement.value.actions
-      resources = statement.value.resources
+      sid           = try(statement.value.sid, null)
+      actions       = try(statement.value.actions, null)
+      not_actions   = try(statement.value.not_actions, null)
+      effect        = try(statement.value.effect, null)
+      resources     = try(statement.value.resources, null)
+      not_resources = try(statement.value.not_resources, null)
 
-      principals {
-        type        = "Service"
-        identifiers = statement.value.principals
+      dynamic "principals" {
+        for_each = try(statement.value.principals, [])
+
+        content {
+          type        = principals.value.type
+          identifiers = principals.value.identifiers
+        }
+      }
+
+      dynamic "not_principals" {
+        for_each = try(statement.value.not_principals, [])
+
+        content {
+          type        = not_principals.value.type
+          identifiers = not_principals.value.identifiers
+        }
+      }
+
+      dynamic "condition" {
+        for_each = try(statement.value.conditions, [])
+
+        content {
+          test     = condition.value.test
+          values   = condition.value.values
+          variable = condition.value.variable
+        }
       }
     }
   }
