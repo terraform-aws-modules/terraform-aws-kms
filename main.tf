45f32bbbc915fb70427c6828354a3bd3ec08f758
magreenbaum <magreenbaum>
2023-09-03 16:45:21 -0400

    add ReplicateKey and ImportKeyMaterial permissions

--- main.tf
+++ main.tf
@@ -139,6 +139,8 @@ data "aws_iam_policy_document" "this" {
         "kms:UntagResource",
         "kms:ScheduleKeyDeletion",
         "kms:CancelKeyDeletion",
+        "kms:ReplicateKey",
+        "kms:ImportKeyMaterial"
       ]
       resources = ["*"]
 
